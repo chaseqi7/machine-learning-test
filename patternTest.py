4ce4621c7ed18709ded02ae587bb3fb9679fdc09
Chase Qi <qiziming1@gmail.com>
2017-03-23 18:02:54 -0400

    MBSP twitter test

--- patternTest.py
+++ patternTest.py
@@ -1,1 +1,5 @@
-import MBSP
+from pattern.web import Twitter, plaintext
+
+twitter = Twitter(language='en')
+for tweet in twitter.search('"more important than"', cached=False):
+    print plaintext(tweet.text)
